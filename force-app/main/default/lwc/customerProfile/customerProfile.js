4fd6909e4a1653c8cd43a701e4b50b22865547ad
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-03-05 08:09:33 +0530

    added an email service class

--- force-app/main/default/lwc/customerProfile/customerProfile.js
+++ force-app/main/default/lwc/customerProfile/customerProfile.js
@@ -1,34 +1,49 @@
 import { LightningElement, wire } from 'lwc';
 import { CurrentPageReference } from 'lightning/navigation';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import getDecryptedRecordId from '@salesforce/apex/EncryptDecryptHandler.getDecryptedRecordId';
 
 export default class CustomerProfile extends LightningElement {
 
-    recordId;
+  recordId;
 
-    @wire(CurrentPageReference)
+    // @wire(CurrentPageReference)
+    // getStateParams(currentPageReference) {
+    //     console.log('inside page reference wire method');
+    //     if (currentPageReference) {
+    //         const encryptedRecordId = currentPageReference.state.recordId;
+    //         console.log('encryptedRecordId---->', encryptedRecordId);
+    //         if (encryptedRecordId) {
+    //             getDecryptedRecordId({ encryptedId: encryptedRecordId })
+    //                 .then(result => {
+    //                     this.recordId = result;
+    //                     console.log('this.recordId---->', this.recordId);
+    //                 })
+    //                 .catch(error => {
+    //                     console.error('Error decrypting record Id', error);
+    //                 });
+    //         }
+    //     }
+    // }
+
+     @wire(CurrentPageReference)
     getStateParams(currentPageReference) {
         console.log('inside page reference wire method');
         if (currentPageReference) {
-            const url = new URL(window.location.href);
-            const recordId = url.searchParams.get('record-id');
-            if (recordId) {
-                this.recordId = recordId;
-                //console.log('this.recordId----->', this.recordId);
-            }
+            this.recordId = currentPageReference.state.recordId;   
         }
     }
-    
+
     handleSave() {
-        console.log("inside save");
-        this.template.querySelector("lightning-record-edit-form").submit();
+        console.log('inside save');
+        this.template.querySelector('lightning-record-edit-form').submit();
     }
 
     handleSuccess() {
         const event = new ShowToastEvent({
-            title: "Success",
-            message: "Updated Successfully",
-            variant: "success"
+            title: 'Success',
+            message: 'Updated Successfully',
+            variant: 'success'
         });
         this.dispatchEvent(event);
     }
