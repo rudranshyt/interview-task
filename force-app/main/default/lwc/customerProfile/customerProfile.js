21423bd9e7ee8dd9680504e62a602c788216efc5
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2024-03-06 08:11:05 +0530

    added custom record editing

--- force-app/main/default/lwc/customerProfile/customerProfile.js
+++ force-app/main/default/lwc/customerProfile/customerProfile.js
@@ -1,11 +1,84 @@
 import { LightningElement, wire } from 'lwc';
+import TshirtFieldValues from '@salesforce/apex/accountPicklistController.TshirtFieldValues';
+import shoeSizeFieldValues from '@salesforce/apex/accountPicklistController.shoeSizeFieldValues';
 import { CurrentPageReference } from 'lightning/navigation';
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getDecryptedRecordId from '@salesforce/apex/EncryptDecryptHandler.getDecryptedRecordId';
+import UpdateRecords from '@salesforce/apex/RecordUpdateHelper.UpdateRecords';
 
 export default class CustomerProfile extends LightningElement {
 
-  recordId;
+    recordId;
+    phoneNumber;
+    tshirtValues = [];
+    shoeSizeValues = [];
+    tshirtSize;
+    shoeSize;
+    dob;
+    
+    connectedCallback() {
+        TshirtFieldValues().then((result) => {
+            this.tshirtValues = result;
+            console.log('this.tshirtValues------>', JSON.stringify(this.tshirtValues));
+        })
+        shoeSizeFieldValues().then((result) => {
+            this.shoeSizeValues = result;
+        })
+    }
+
+    @wire(CurrentPageReference)
+    getStateParams(currentPageReference) {
+        console.log('inside page reference wire method');
+        if (currentPageReference) {
+            this.recordId = currentPageReference.state.recordId;   
+        }
+    }
+
+    handleShoeSize(event) {
+        this.shoeSize = event.target.value;
+    }
+   
+    handleTshirtValue(event) {
+        this.tshirtSize = event.target.value;
+    }
+
+    handlePhoneChange(event) {
+        this.phoneNumber = event.target.value;
+    }
+
+    handleDob(event) {
+        this.dob = event.target.value;
+    }
+
+    handleSave() {
+        UpdateRecords({
+            phone: this.phoneNumber,
+            tshirtsize: this.tshirtSize,
+            shoesize: this.shoeSize,
+            dob: this.dob,
+            recordId: this.recordId
+        })
+        .then(result => {
+            
+            console.log('Record updated successfully');
+            this.showToast("Success", "Updated successfully", "success");
+        })
+        .catch(error => {
+            this.showToast("Error", "could not update", "error");
+            console.error('Error updating record---->', error);
+        });
+    }
+
+
+    showToast(title, message, variant) {
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title: title,
+        message: message,
+        variant: variant
+      })
+    );
+  }
 
     // @wire(CurrentPageReference)
     // getStateParams(currentPageReference) {
@@ -26,25 +99,21 @@ export default class CustomerProfile extends LightningElement {
     //     }
     // }
 
-     @wire(CurrentPageReference)
-    getStateParams(currentPageReference) {
-        console.log('inside page reference wire method');
-        if (currentPageReference) {
-            this.recordId = currentPageReference.state.recordId;   
-        }
-    }
+    
 
-    handleSave() {
-        console.log('inside save');
-        this.template.querySelector('lightning-record-edit-form').submit();
-    }
+    // handleSave() {
+    //     console.log('inside save');
+    //     this.template.querySelector('lightning-record-edit-form').submit();
+    // }
 
-    handleSuccess() {
-        const event = new ShowToastEvent({
-            title: 'Success',
-            message: 'Updated Successfully',
-            variant: 'success'
-        });
-        this.dispatchEvent(event);
-    }
+    // handleSuccess() {
+    //     const event = new ShowToastEvent({
+    //         title: 'Success',
+    //         message: 'Updated Successfully',
+    //         variant: 'success'
+    //     });
+    //     this.dispatchEvent(event);
+    // }
+
+
 }
